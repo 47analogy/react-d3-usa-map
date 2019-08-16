e2ff4f2ef71c1681cc4e76f3e720c742828d308b
analogy47 <actweed47@gmail.com>
2019-08-16 01:39:13 -0400

    change map colors

--- components/DisplayMap.js
+++ components/DisplayMap.js
@@ -1,7 +1,7 @@
 import * as d3 from 'd3';
 
 const DisplayMap = props => {
-	const { coords, svgHeight, svgWidth, toggleMapClick } = props;
+	const { coords, svgHeight, svgWidth, toggleMapVotes } = props;
 
 	const projection = () => {
 		return d3.geoAlbersUsa();
@@ -10,14 +10,20 @@ const DisplayMap = props => {
 	return (
 		<div>
 			<svg width={svgWidth} height={svgHeight}>
-				<g className='states'>
+				<g className="states">
 					{coords.map((d, i) => (
 						<path
 							key={`path-${i}`}
-							onClick={() => toggleMapClick(d)}
+							onClick={() => toggleMapVotes(d)}
 							d={d3.geoPath().projection(projection())(d)}
-							className='state'
-							fill={d.properties.WINNER === 1 ? '#FF5252' : '#2096F3'}
+							className="state"
+							fill={
+								!d.properties.WINNER
+									? '#F4CB43'
+									: d.properties.WINNER === 1
+									? '#EB4C50'
+									: '#1A9AF2'
+							}
 							stroke={'#000000'}
 						/>
 					))}
